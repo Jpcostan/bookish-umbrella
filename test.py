900ce5fe34b43f7beba97f1acf7af8ba851638f3
Joshua ELP <costanzajosh@gmail.com>
2022-03-15 20:42:16 -0400

    Added initial Python src file
    blah blah blah la de da

---
+++ test.py
@@ -0,0 +1,24 @@
+print("Welcome to my python program! WOW")
+
+#_age var
+
+#_name var
+
+#_isCool var
+
+
+#print your age
+def printAge():
+ 
+#print your name   
+def printName():
+
+#print true/false if you are cool or not    
+def printCoolStatus():
+    #print
+    #if cool, then print "Yes i am cool"
+    #if not cool, print, "totally not cool"
+
+
+
+
